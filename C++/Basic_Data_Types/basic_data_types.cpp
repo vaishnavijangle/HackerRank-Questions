4d2f8989d0ff089abc329d22f9f8e8ea2dd8ef52
Vaishnavi Jangle <vaishnavijangle1603@gmail.com>
2021-04-25 16:26:36 +0530

    Add C++ programs.

---
+++ C++/Basic_Data_Types/basic_data_types.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Complete the code.
+	int i;
+	long l;
+	char c;
+	float f;
+	double d;
+	//scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+	cin >> i >> l >> c >> f >> d;
+	printf("%d\n", i);
+	printf("%ld\n", l);
+	printf("%c\n", c);
+	printf("%f\n", f);
+	printf("%lf\n", d);
+	return 0;
+}
---
+++ C++/Input_and_Output/input_output.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << ("Hi");
+	if (1 <= a <= 1000 && 1 <= b <= 1000 && 1 <= c <= 1000)
+	{
+		cout << (a + b + c);
+	}
+
+	return 0;
+}
